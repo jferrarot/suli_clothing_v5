ce2a9c33fb3713e012a91f7a8b9ec02d55fd5b6f
jferrarot <jferrarot@gmail.com>
2022-08-03 17:55:17 -0500

    checkout-item

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,36 +1,41 @@
-
 import { useContext } from 'react';
-
 import { CartContext } from '../../contexts/cart.context';
+import {
+    Arrow,
+    CheckoutItemContainer,
+    Image,
+    Name,
+    Price,
+    Quantity,
+    RemoveButton,
+    Value,
+} from './checkout-item.styles';
 
-import './checkout-item.styles.scss';
+const CheckoutItem = ({ cartItem }) => {
+    const { imageUrl, name, quantity, price } = cartItem;
 
-const CheckoutItem = ({cartItem}) =>{
+    const { addItemToCart, removeItemFromCart, clearItemFromCart } =
+        useContext(CartContext);
 
-  const {imageUrl,name,quantity,price} = cartItem;
+    const clearItemHandler = () => clearItemFromCart(cartItem);
+    const addItemHandler = () => addItemToCart(cartItem);
+    const removeItemHandler = () => removeItemFromCart(cartItem);
 
-  const {addItemToCart,removeItemFromCart,clearItemFromCart} = useContext(CartContext);
-
-  const clearItemHandler = () => clearItemFromCart(cartItem);
-  const addItemHandler = () =>addItemToCart(cartItem);
-  const removeItemHandler = () =>removeItemFromCart(cartItem);
-
-  return(
-    <div className='checkout-item-container'>
-      <div className='image-container'>
-        <img src={imageUrl} alt={`${name}`} />
-      </div>
-      <span className='name'>{name}</span>
-      <span className='quantity'>
-        <div className='arrow' onClick={removeItemHandler}>&#10094;</div>
-        <span className='value'>{quantity}</span>
-        <div className='arrow' onClick={addItemHandler}>&#10095;</div>
-      </span>
-      <span className='price'>{price}</span>
-      <div className='remove-button' onClick={clearItemHandler}>&#10005;</div>
-    </div>
-  );
-
+    return (
+        <CheckoutItemContainer>
+            <Image>
+                <img src={imageUrl} alt={`${name}`} />
+            </Image>
+            <Name>{name}</Name>
+            <Quantity>
+                <Arrow onClick={removeItemHandler}>&#10094;</Arrow>
+                <Value>{quantity}</Value>
+                <Arrow onClick={addItemHandler}>&#10095;</Arrow>
+            </Quantity>
+            <Price>{price}</Price>
+            <RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
+        </CheckoutItemContainer>
+    );
 };
 
 export default CheckoutItem;
