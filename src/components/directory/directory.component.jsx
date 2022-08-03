7098efb8382dc1c6d51fc2b1dbd4b77b64d335ed
jferrarot <jferrarot@gmail.com>
2022-08-03 17:57:21 -0500

    directory container

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,13 +1,13 @@
 import DirectoryItem from '../directory-item/directory-item.component';
-import './directory.styles.scss';
+import { DirectoryContainer } from './directory.styles';
 
 const Directory = ({ categories }) => {
     return (
-        <div className="directory-container">
+        <DirectoryContainer>
             {categories.map((directory) => (
                 <DirectoryItem key={directory.id} directory={directory} />
             ))}
-        </div>
+        </DirectoryContainer>
     );
 };
 
