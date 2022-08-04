b6c8c777314a7afc81a41c9763ff958009522507
jferrarot <jferrarot@gmail.com>
2022-08-04 13:41:32 -0500

    reducer.utils.jsx

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useReducer } from 'react';
+import { createAction } from '../utils/reducer/reducer.utils';
 import {
     createUserDocumentFromAuth,
     onAuthStateChangedListener,
@@ -36,7 +37,7 @@ export const UserProvider = ({ children }) => {
     //const {currentUser} = state;
 
     const setCurrentUser = (user) => {
-        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
     };
 
     const value = { currentUser, setCurrentUser };
