658dba6d716269b5e6b517f4bf2e0ecae5988d12
jferrarot <jferrarot@gmail.com>
2022-08-24 13:24:49 -0500

    migrate categories redux js to typescript;

--- src/store/categories/categories.selector.ts
+++ src/store/categories/categories.selector.ts
@@ -1,8 +1,10 @@
 import { createSelector } from 'reselect';
 
-const selectCategoryReducer = (state) => {
-    return state.categories;
-};
+import { CategoriesState } from './categories.reducer';
+import { CategoryMap } from './categories.types';
+
+const selectCategoryReducer = (state): CategoriesState => state.categories;
+
 export const selectCategories = createSelector(
     [selectCategoryReducer],
     (categoriesSlice) => categoriesSlice.categories
@@ -10,12 +12,12 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
     [selectCategories],
-    (categories) =>
+    (categories): CategoryMap =>
         categories.reduce((acc, category) => {
             const { title, items } = category;
             acc[title.toLowerCase()] = items;
             return acc;
-        }, {})
+        }, {} as CategoryMap)
 );
 
 export const selectCategoriesIsLoading = createSelector(
